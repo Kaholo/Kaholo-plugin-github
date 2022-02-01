120652f11f18dd6fbef0c9eb625a5ddb4fb6d20d
Matan Kadosh <kadosh10001@gmail.com>
2022-02-01 16:32:54 +0200

    add new method 'setBranchProtectionRule'

--- src/github.service.js
+++ src/github.service.js
@@ -1,107 +1,107 @@
-const { sendToGithub, listGithubRequest, getRepo} = require("./helpers");
+const { sendToGithub, listGithubRequest, getRepo } = require("./helpers");
 const parsers = require("./parsers");
 
 async function sendStatus(action, settings) {
-    const {state, linkedUrl, description, context} = action.params;
+    const { state, linkedUrl, description, context } = action.params;
     const repo = getRepo(action.params);
     const sha = parsers.autocomplete(action.params.sha);
     const token = action.params.token || settings.token;
-    if (!token || !sha || !state){
-        throw("One of required parameters was not given");
+    if (!token || !sha || !state) {
+        throw "One of required parameters was not given";
     }
     const reqUrl = `/repos/${repo}/statuses/${sha}`;
     const body = {
         state: state,
         target_url: parsers.string(linkedUrl),
         description: parsers.string(description),
-        context: parsers.string(context)
+        context: parsers.string(context),
     };
-    
+
     const res = await sendToGithub(reqUrl, "POST", token, body);
-    if (res.message && res.message.includes("No commit found for SHA")){
+    if (res.message && res.message.includes("No commit found for SHA")) {
         throw res;
     }
     return res;
 }
 
 async function createRepo(action, settings) {
-    const {name, private, autoInit, gitignoreTemplate, description} = action.params;
+    const { name, private, autoInit, gitignoreTemplate, description } = action.params;
     const owner = parsers.autocomplete(action.params.owner);
     const token = action.params.token || settings.token;
-    if (!token || !name){
-        throw("One of required parameters was not given");
+    if (!token || !name) {
+        throw "One of required parameters was not given";
     }
     const reqPath = owner && owner !== "user" ? `/orgs/${owner}/repos` : `/user/repos`;
     const body = {
-        name: parsers.string(name), 
+        name: parsers.string(name),
         private: parsers.boolean(private),
         auto_init: parsers.boolean(autoInit),
         gitignore_template: parsers.string(gitignoreTemplate),
-        description: parsers.string(description)
+        description: parsers.string(description),
     };
-    
-    return sendToGithub(reqPath, "POST", token, body);;
+
+    return sendToGithub(reqPath, "POST", token, body);
 }
 
 async function createRepoFromTemplate(action, settings) {
-    const {templateRepo, name, description, private} = action.params;
+    const { templateRepo, name, description, private } = action.params;
     const owner = parsers.autocomplete(action.params.owner);
     const token = action.params.token || settings.token;
-    if (!token || !templateRepo || !name){
-        throw("One of required parameters was not given");
+    if (!token || !templateRepo || !name) {
+        throw "One of required parameters was not given";
     }
-    if (!templateRepo.includes("/")){
-        throw(`Bad template repository name format.
-Repository Name should be in the format of {owner}/{repo}`);
+    if (!templateRepo.includes("/")) {
+        throw `Bad template repository name format.
+Repository Name should be in the format of {owner}/{repo}`;
     }
     const reqPath = `/repos/${templateRepo}/generate`;
     const body = {
-        name: parsers.string(name), 
+        name: parsers.string(name),
         private: parsers.boolean(private),
         owner: owner === "user" ? undefined : owner,
-        description: parsers.string(description)
-    }
-    
-    return sendToGithub(reqPath, "POST", token, body);;
+        description: parsers.string(description),
+    };
+
+    return sendToGithub(reqPath, "POST", token, body);
 }
 
 async function createRepoWebhook(action, settings) {
-    const {url, secret, insecureSsl, notActive} = action.params;
+    const { url, secret, insecureSsl, notActive } = action.params;
     const repo = getRepo(action.params);
     const contentType = action.params.contentType || "json";
     const events = parsers.array(action.params.events);
     const token = action.params.token || settings.token;
     if (!token || !url) {
-        throw("One of required parameters was not given");
+        throw "One of required parameters was not given";
     }
     const reqPath = `/repos/${repo}/hooks`;
     const body = {
-        name: "web", 
+        name: "web",
         config: {
             url: parsers.string(url),
             content_type: parsers.string(contentType),
             insecure_ssl: parsers.boolean(insecureSsl) ? 1 : 0,
-            secret: secret
+            secret: secret,
         },
         events: events.length > 0 ? events : undefined,
-        active: !parsers.boolean(notActive)
-    }
-    
-    return sendToGithub(reqPath, "POST", token, body);;
+        active: !parsers.boolean(notActive),
+    };
+
+    return sendToGithub(reqPath, "POST", token, body);
 }
 
 async function createOrganizationWebhook(action, settings) {
-    const {url, secret, insecureSsl, notActive, username, password} = action.params;
+    const { url, secret, insecureSsl, notActive, username, password } = action.params;
     const org = parsers.autocomplete(action.params.org);
     const contentType = action.params.contentType || "json";
     const events = parsers.array(action.params.events);
     const token = action.params.token || settings.token;
     if (!token || !org || !url) {
-        throw("One of required parameters was not given");
+        throw "One of required parameters was not given";
     }
     const reqPath = `/orgs/${org}/hooks`;
     const body = {
-        name: "web", 
+        name: "web",
         config: {
             url: parsers.string(url),
             content_type: parsers.string(contentType),
@@ -111,28 +111,52 @@ async function createOrganizationWebhook(action, settings) {
             password: parsers.string(password),
         },
         events: events.length > 0 ? events : undefined,
-        active: !parsers.boolean(notActive)
-    }
-    
-    return sendToGithub(reqPath, "POST", token, body);;
+        active: !parsers.boolean(notActive),
+    };
+
+    return sendToGithub(reqPath, "POST", token, body);
+}
+
+async function setBranchProtectionRule(action, settings) {
+    const params = action.params;
+    const repo = getRepo(params);
+    const reqPath = `/repos/${repo}/branches/${params.branch}/protection`;
+    const body = {
+        required_status_checks: { 
+            strict: true, 
+            contexts: Array.isArray(params.contexts) ? params.contexts : params.contexts.split('\n')
+        },
+        enforce_admins: parsers.boolean(params.enforceAdmins),
+        required_pull_request_reviews: {
+            dismissal_restrictions: { users: [], teams: [] },
+            dismiss_stale_reviews: parsers.boolean(params.dismissStaleReviews),
+            require_code_owner_reviews: parsers.boolean(params.requireOwnerReview),
+            required_approving_review_count: parsers.number(params.approvingCount),
+            bypass_pull_request_allowances: { users: [], teams: [] },
+        },
+        restrictions: null
+    };
+
+    const token = action.params.token || settings.token;
+    return sendToGithub(reqPath, "PUT", token, body);
 }
 
 async function listOrgs(params, settings) {
     return listGithubRequest(params, settings, "/user/orgs");
 }
-    
+
 async function getAuthenticatedUser(params, settings) {
     return sendToGithub("/user", "GET", params.token || settings.token);
 }
-    
+
 async function getRepository(params, settings) {
     return sendToGithub(`/repos/${getRepo(params)}`, "GET", params.token || settings.token);
 }
 
 async function getPullRequest(params, settings) {
     const pr = parsers.pullRequest(params.pr);
-    if (!pr || pr.paramType !== "pulls"){
-        throw "Must provide the pull request either from autocomplete or it's ID from code."
+    if (!pr || pr.paramType !== "pulls") {
+        throw "Must provide the pull request either from autocomplete or it's ID from code.";
     }
     if (pr.url) return sendToGithub(pr.url.pathname, "GET", params.token || settings.token);
     return sendToGithub(`/repos/${getRepo(params)}/pulls/${pr.id}`, "GET", params.token || settings.token);
@@ -141,21 +165,20 @@ async function getPullRequest(params, settings) {
 async function postPRComment(params, settings) {
     const pr = parsers.pullRequest(params.pr);
     const comment = parsers.string(params.comment);
-    if (!pr || !comment){
-        throw("One of required parameters was not given");
+    if (!pr || !comment) {
+        throw "One of required parameters was not given";
     }
-    var path;    
-    if (pr.paramType === "issues"){
+    var path;
+    if (pr.paramType === "issues") {
         path = pr.url.pathname;
-        if (!path.endsWith("/comments")){
+        if (!path.endsWith("/comments")) {
             path += "/comments";
         }
-    }
-    else{
+    } else {
         const pullRequest = await getPullRequest(params, settings);
-        path = (new URL(pullRequest.comments_url)).pathname;
+        path = new URL(pullRequest.comments_url).pathname;
     }
-    return sendToGithub(path, "POST", params.token || settings.token, {body: comment});
+    return sendToGithub(path, "POST", params.token || settings.token, { body: comment });
 }
 
 async function listRepos(params, settings) {
@@ -174,7 +197,7 @@ async function listBranches(params, settings) {
 async function listCommits(params, settings) {
     const repo = getRepo(params);
     const branch = parsers.autocomplete(params.branch);
-    return listGithubRequest(params, settings, `/repos/${repo}/commits`, {sha: branch});
+    return listGithubRequest(params, settings, `/repos/${repo}/commits`, { sha: branch });
 }
 
 async function listPullRequests(params, settings) {
@@ -189,13 +212,14 @@ module.exports = {
     createRepoWebhook,
     createOrganizationWebhook,
     postPRComment,
+    setBranchProtectionRule,
     // list/get funcs
-    getAuthenticatedUser, 
+    getAuthenticatedUser,
     getRepository,
     getPullRequest,
     listOrgs,
     listRepos,
     listBranches,
     listCommits,
-    listPullRequests
-}
+    listPullRequests,
+};
